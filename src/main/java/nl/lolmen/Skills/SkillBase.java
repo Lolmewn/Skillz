67d3b4178f73f29cfad69c819d9894805b3f97a3
Lolmewn <Lolmewn@SybrensPC.lan>
2012-01-01 11:27:30 +0100

    Possible fix for the removal of Skillz files.

--- src/main/java/nl/lolmen/Skills/SkillBase.java
+++ src/main/java/nl/lolmen/Skills/SkillBase.java
@@ -21,58 +21,45 @@ public class SkillBase {
 	private int multiplier;
 	private boolean enabled;
 	File folder = new File("plugins" + File.separator + "Skillz" + File.separator + "players" + File.separator);
-	
+
 	public void addXP(final Player p, final int XP) {
 		final SkillBase skill = this;
-		Bukkit.getScheduler().scheduleAsyncDelayedTask(Skillz.p, new Runnable() {
-			public void run() {
-				if(CPU.list.contains(p.getName())){
-					waitForIt(p);
-				}else{
-					CPU.list.add(p.getName());
-				}
-				int xp;
-				int lvl;
-				try {
-					if(!new File(folder, p.getName().toLowerCase() + ".txt").exists()){
-						new File(folder, p.getName().toLowerCase() + ".txt").createNewFile();
-						Skillz.p.log.info("New file created for " + p.getName());
-					}
-					String skillname = skill.getSkillName().toLowerCase();
-					Properties prop = new Properties();
-					FileInputStream in = new FileInputStream(new File(folder, p.getName().toLowerCase() + ".txt"));
-					prop.load(in);
-					if (!prop.containsKey(skillname)) {
-						System.out.println("Skill " + skillname + " not found, adding!");
-						prop.put(skillname, "0;0");
-						prop.store(new FileOutputStream(new File(folder, p
-								.getName().toLowerCase() + ".txt")),
-								"Skill=XP;lvl");
-					}
-					String get = prop.getProperty(skillname);
-					String[] array = get.split(";");
-					xp = Integer.parseInt(array[0]);
-					lvl = Integer.parseInt(array[1]);
-					prop.setProperty(skillname,Integer.toString(xp + XP) + ";"+ array[1]);
-					FileOutputStream out = new FileOutputStream(new File(
-							folder, p.getName().toLowerCase() + ".txt"));
-					prop.store(out, "Skill=XP;lvl");
-					CPU.checkLeveling(p, skill, lvl, (xp + XP));
-					in.close();
-					out.flush();
-					out.close();
-					CPU.list.remove(p.getName());
-					return;
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+		int xp;
+		int lvl;
+		try {
+			if(!new File(folder, p.getName().toLowerCase() + ".txt").exists()){
+				new File(folder, p.getName().toLowerCase() + ".txt").createNewFile();
+				Skillz.p.log.info("New file created for " + p.getName());
 			}
-			private void waitForIt(Player p ) {
-				while(CPU.list.contains(p.getName())){
-				}
+			String skillname = skill.getSkillName().toLowerCase();
+			Properties prop = new Properties();
+			FileInputStream in = new FileInputStream(new File(folder, p.getName().toLowerCase() + ".txt"));
+			prop.load(in);
+			if (!prop.containsKey(skillname)) {
+				System.out.println("Skill " + skillname + " not found, adding!");
+				prop.put(skillname, "0;0");
+				prop.store(new FileOutputStream(new File(folder, p
+						.getName().toLowerCase() + ".txt")),
+						"Skill=XP;lvl");
 			}
-		}, 1L);
+			String get = prop.getProperty(skillname);
+			String[] array = get.split(";");
+			xp = Integer.parseInt(array[0]);
+			lvl = Integer.parseInt(array[1]);
+			prop.setProperty(skillname,Integer.toString(xp + XP) + ";"+ array[1]);
+			FileOutputStream out = new FileOutputStream(new File(
+					folder, p.getName().toLowerCase() + ".txt"));
+			prop.store(out, "Skill=XP;lvl");
+			CPU.checkLeveling(p, skill, lvl, (xp + XP));
+			in.close();
+			out.flush();
+			out.close();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
 
 	public String getSkillName() {
 		return skillName.toLowerCase();
