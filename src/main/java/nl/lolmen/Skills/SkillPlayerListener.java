052a200447d313a5213e2bfc6e036825092ce4d7
Lolmewn <Lolmewn@SybrensPC>
2012-04-30 16:39:40 +0200

    Don't reload player profile on re-join

--- src/main/java/nl/lolmen/Skills/SkillPlayerListener.java
+++ src/main/java/nl/lolmen/Skills/SkillPlayerListener.java
@@ -28,62 +28,12 @@ public class SkillPlayerListener implements Listener {
                 this.plugin.getLogger().info("[DEBUG] Asked " + p.getName() + " to config Skillz");
             }
         }
-        
-        this.plugin.getUserManager().loadPlayer(p.getName());
+        if(!this.plugin.getUserManager().hasPlayer(p.getName())){
+            this.plugin.getUserManager().loadPlayer(p.getName());
+        }        
         double time2 = System.nanoTime();
-                double taken = (time2 - time) / 1000000.0D;
-                this.plugin.getLogger().info("Loaded player in " + Double.toString(taken) + "ms!");
-        /*
-        if (!this.plugin.useMySQL && (!new File(plugin.maindir + "players/" + p.getName().toLowerCase() + ".txt").exists())) {
-            try {
-                this.plugin.getLogger().info("[Skillz] File created for " + p.getName().toLowerCase() + "!");
-                new File(plugin.maindir + "players/" + p.getName().toLowerCase() + ".txt").createNewFile();
-                Properties prop = new Properties();
-                FileInputStream in = new FileInputStream(new File(plugin.maindir + "players/" + p.getName().toLowerCase() + ".txt"));
-                prop.load(in);
-                for (SkillBase skill : this.plugin.getSkillManager().getSkills()) {
-                    if (!skill.isEnabled()) {
-                        continue;
-                    }
-                    prop.put(skill.getSkillName(), "0;0");
-                }
-                FileOutputStream out = new FileOutputStream(new File(this.plugin.maindir + "players/" + p.getName().toLowerCase() + ".txt"));
-                prop.store(out, "Skill=XP;lvl");
-                in.close();
-                out.flush();
-                out.close();
-                double time2 = System.nanoTime();
-                double taken = (time2 - time) / 1000000.0D;
-                this.plugin.getLogger().info("Creation took " + Double.toString(taken) + "ms!");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        if (this.plugin.useMySQL) {
-            try {
-                String query = "SELECT * FROM " + this.plugin.dbTable + " WHERE player='" + p.getName() + "';";
-                ResultSet res = this.plugin.getMySQL().executeQuery(query);
-                if (res != null) {
-                    while (res.next()) {
-                        return; //is in the database, no need to add him
-                    }
-                    //not in the DB after all
-                }
-                for (SkillBase skill : plugin.getSkillManager().getSkills()) {
-                    if (!skill.isEnabled()) {
-                        continue;
-                    }
-                    this.plugin.getMySQL().executeQuery("INSERT INTO " + this.plugin.dbTable + " (player, skill, xp, level) VALUES ('" + p.getName() + "', '" + skill.getSkillName() + "', 0, 0);");
-                }
-                this.plugin.getLogger().info("[Skillz] MySQL Entry created for " + p.getName() + "!");
-                double time2 = System.nanoTime();
-                double taken = (time2 - time) / 1000000.0D;
-                this.plugin.getLogger().info("Creation took " + Double.toString(taken) + "ms!");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }*/
-        
+        double taken = (time2 - time) / 1000000.0D;
+        this.plugin.getLogger().info("Loaded player in " + Double.toString(taken) + "ms!");
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
