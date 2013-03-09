1ec04d58a97766f41fbe336f3f6a8d2569648e63
Lolmewn <Lolmewn@SybrensPC>
2013-03-09 20:24:07 +0100

    Some changes

--- src/main/java/nl/lolmen/Skills/skills/CustomSkill.java
+++ src/main/java/nl/lolmen/Skills/skills/CustomSkill.java
@@ -1,96 +1,92 @@
 package nl.lolmen.Skills.skills;
 
 import java.util.HashMap;
-
-import org.bukkit.block.Block;
-
 import nl.lolmen.Skills.SkillBase;
 import nl.lolmen.Skillz.Skillz;
+import org.bukkit.block.Block;
 
-public class CustomSkill extends SkillBase{
-	
-	public CustomSkill(Skillz plugin) {
-		super(plugin);
-	}
+public class CustomSkill extends SkillBase {
 
-	private String uses;
-	private HashMap<Integer, Integer> blocks = new HashMap<Integer, Integer>();
-	private HashMap<Integer, Integer> blockLevels = new HashMap<Integer, Integer>();
+    public CustomSkill(Skillz plugin) {
+        super(plugin);
+    }
+    private String uses;
+    private HashMap<Integer, Integer> blocks = new HashMap<Integer, Integer>();
+    private HashMap<Integer, Integer> blockLevels = new HashMap<Integer, Integer>();
 
-	public String getUses() {
-		return uses;
-	}
-	
-	public String[] getUsesArray(){
-		if(!uses.contains(",")){
-			return new String[]{uses};
-		}
-		return uses.split(",");
-	}
+    public String getUses() {
+        return uses;
+    }
 
-	public void setUses(String uses) {
-		this.uses = uses;
-	}
-	
-	public boolean hasUse(String eventType){
-		if(!uses.contains(",")){
-			return uses.equalsIgnoreCase(eventType) ? true : false;
-		}
-		for(String use : uses.split(",")){
-			if(use.equalsIgnoreCase(eventType)){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public HashMap<Integer, Integer> getBlockLevels() {
-		return blockLevels;
-	}
-	
-	public void addBlockLevels(int block, int level){
-		blockLevels.put(block, level);
-	}
-	
-	public void addBlock(int block, int xp){
-		blocks.put(block, xp);
-	}
-	
-	public void setBlocks(HashMap<Integer, Integer> blocks) {
-		this.blocks = blocks;
-	}
-	
-	/**
-	 * @param data BlockData 
-	 */
-	public boolean hasBlock(int block, byte data){
-		return blocks.containsKey(block);
-	}
-	
-	public boolean hasBlock(Block b){
-		return hasBlock(b.getTypeId(), b.getData());
-	}
-	
-	public int getXP(int block){
-		if(blocks.containsKey(block)){
-			return blocks.get(block);
-		}
-		return 0;
-	}
-	
-	public int getXP(Block block){
-		return this.getXP(block.getTypeId());
-	}
-	
-	public int getLevelNeeded(int blockID){
-		if(blockLevels.containsKey(blockID)){
-			return blockLevels.get(blockID);
-		}
-		return 0;
-	}
-	
-	public int getLevelNeeded(Block b){
-		return getLevelNeeded(b.getTypeId());
-	}
+    public String[] getUsesArray() {
+        if (!uses.contains(",")) {
+            return new String[]{uses};
+        }
+        return uses.split(",");
+    }
 
+    public void setUses(String uses) {
+        this.uses = uses;
+    }
+
+    public boolean hasUse(String eventType) {
+        if (!uses.contains(",")) {
+            return uses.equalsIgnoreCase(eventType) ? true : false;
+        }
+        for (String use : uses.split(",")) {
+            if (use.equalsIgnoreCase(eventType)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public HashMap<Integer, Integer> getBlockLevels() {
+        return blockLevels;
+    }
+
+    public void addBlockLevels(int block, int level) {
+        blockLevels.put(block, level);
+    }
+
+    public void addBlock(int block, int xp) {
+        blocks.put(block, xp);
+    }
+
+    public void setBlocks(HashMap<Integer, Integer> blocks) {
+        this.blocks = blocks;
+    }
+
+    /**
+     * @param data BlockData
+     */
+    public boolean hasBlock(int block, byte data) {
+        return blocks.containsKey(block);
+    }
+
+    public boolean hasBlock(Block b) {
+        return hasBlock(b.getTypeId(), b.getData());
+    }
+
+    public int getXP(int block) {
+        if (blocks.containsKey(block)) {
+            return blocks.get(block);
+        }
+        return 0;
+    }
+
+    public int getXP(Block block) {
+        return this.getXP(block.getTypeId());
+    }
+
+    public int getLevelNeeded(int blockID) {
+        if (blockLevels.containsKey(blockID)) {
+            return blockLevels.get(blockID);
+        }
+        return 0;
+    }
+
+    public int getLevelNeeded(Block b) {
+        return getLevelNeeded(b.getTypeId());
+    }
 }
